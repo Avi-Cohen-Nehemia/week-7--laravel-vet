6bf3aec2c69121ee483d2225f2c971bc71062fbc
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-05-15 14:38:57 +0100

    added search bar in the navbar

--- resources/views/parts/nav.blade.php
+++ resources/views/parts/nav.blade.php
@@ -1,14 +1,31 @@
-<ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" href="http://192.168.10.10">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="http://192.168.10.10/about">About</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="http://192.168.10.10/phonebook/create">Join</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="http://192.168.10.10/phonebook">Customer Phonebook</a>
-  </li>
-</ul>
+<div class="nav-tabs row justify-content-between">
+    <div class="col-md-6 d-flex align-items-center">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="http://192.168.10.10">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://192.168.10.10/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://192.168.10.10/phonebook/create">Join</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://192.168.10.10/phonebook">Customer Phonebook</a>
+            </li>
+        </ul>
+    </div>
+    <form class="col-md-3">
+        <div class="card-body row no-gutters align-items-center">
+            <div class="col-auto">
+                <i class="fas fa-search h4 text-body"></i>
+            </div>
+            <div class="col">
+                <input class="form-control form-control-md form-control-borderless" type="search" placeholder="Search Owners">
+            </div>
+            <div class="col-auto">
+                <button class="btn btn-md btn-success" type="submit">Search</button>
+            </div>
+        </div>
+    </form>
+</div>
